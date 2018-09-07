e0ccc610a6732381c0cad199691be547672e25d5
George Brande <george.brande@gmail.com>
2018-09-07 14:58:08 +0300

    flask + srv OK

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 from .entities.entity import Session, engine, Base
-from .entities.exam import Exam
+from .entities.exam import Exam, ExamSchema
+
+from flask import Flask, jsonify, request
 
 # generate database schema
 Base.metadata.create_all(engine)
@@ -9,6 +11,8 @@ session = Session()
 
 # check for existing data
 exams = session.query(Exam).all()
+
+app = Flask(__name__)
 
 if len(exams) == 0:
     # create and persist dummy exam
@@ -24,3 +28,32 @@ if len(exams) == 0:
 print('### Exams:')
 for exam in exams:
     print(f'({exam.id}) {exam.title} - {exam.description}')
+
+@app.route('/exams')
+def get_exams():
+    session = Session()
+    exam_objects = session.query(Exam).all()
+
+    schema = ExamSchema(many=True)
+    exams = schema.dump(exam_objects)
+
+    session.close()
+    return jsonify(exams.data)
+
+@app.route('/exams', methods=['POST'])
+def add_exam():
+    # mount exam object
+    posted_exam = ExamSchema(only=('title', 'description'))\
+        .load(request.get_json())
+
+    exam = Exam(**posted_exam.data, created_by="HTTP post request")
+
+    # persist exam
+    session = Session()
+    session.add(exam)
+    session.commit()
+
+    # return created exam
+    new_exam = ExamSchema().dump(exam).data
+    session.close()
+    return jsonify(new_exam), 201
